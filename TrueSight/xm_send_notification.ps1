edd79937cd79b6ed9c447e923b2c256150b05c9f
xm-emaxwell <emaxwell@xmatters.com>
2022-07-04 10:01:58 -0400

    Add user field and updated checkForNull

--- TrueSight/xm_send_notification.ps1
+++ TrueSight/xm_send_notification.ps1
@@ -1,38 +1,39 @@
 $url = "<path to xM trigger URL>"
 
-function checkEnvValue {
+function checkForNull {
 
     param (
-        $EnvValue
+        $value
     )
 
-    if ($EnvValue -ne $null){
-        Write-Output $EnvValue
+    if ($value){
+        Write-Output $value
     }
     else {
         Write-Output ""
     }
-
 }
+
 
 $body = @{
-    "id" = checkEnvValue -EnvValue $env:mc_ueid
-    "severity" = checkEnvValue -EnvValue $env:severity
-    "status" = checkEnvValue -EnvValue $env:status
-    "priority" = checkEnvValue -EnvValue $env:mc_priority
-    "reception_date" = checkEnvValue -EnvValue $env:date_reception
-    "cell_name" = checkEnvValue -EnvValue $env:CELL_NAME
-    "host_address" = checkEnvValue -EnvValue $env:mc_host_address
-    "client_address" = checkEnvValue -EnvValue $env:mc_client_address
-    "host" =  checkEnvValue -EnvValue $env:mc_host
-    "object" =  checkEnvValue -EnvValue $env:mc_object
-    "object_class" =  checkEnvValue -EnvValue $env:mc_object_class
-    "parameters" =  checkEnvValue -EnvValue $env:mc_parameter
-    "tool" =  checkEnvValue -EnvValue $env:mc_tool
-    "tool_class" =  checkEnvValue -EnvValue $env:mc_tool_class
-    "smc_causes" =  checkEnvValue -EnvValue $env:mc_smc_causes
-    "smc_effects" =  checkEnvValue -EnvValue $env:mc_smc_effects
-    "msg" =  checkEnvValue -EnvValue $env:msg
+    "user" = checkForNull -value $args[0]
+    "id" = checkForNull -value $env:mc_ueid
+    "severity" = checkForNull -value $env:severity
+    "status" = checkForNull -value $env:status
+    "priority" = checkForNull -value $env:mc_priority
+    "reception_date" = checkForNull -value $env:date_reception
+    "cell_name" = checkForNull -value $env:CELL_NAME
+    "host_address" = checkForNull -value $env:mc_host_address
+    "client_address" = checkForNull -value $env:mc_client_address
+    "host" =  checkForNull -value $env:mc_host
+    "object" =  checkForNull -value $env:mc_object
+    "object_class" =  checkForNull -value $env:mc_object_class
+    "parameters" =  checkForNull -value $env:mc_parameter
+    "tool" =  checkForNull -value $env:mc_tool
+    "tool_class" =  checkForNull -value $env:mc_tool_class
+    "smc_causes" =  checkForNull -value $env:mc_smc_causes
+    "smc_effects" =  checkForNull -value $env:mc_smc_effects
+    "msg" =  checkForNull -value $env:msg
 }
 
-Invoke-RestMethod -Method 'Post' -Uri $url -Body ($body|ConvertTo-Json) -ContentType "application/json" 
+Invoke-RestMethod -Method 'Post' -Uri $url -Body ($body|ConvertTo-Json) -ContentType "application/json"  
